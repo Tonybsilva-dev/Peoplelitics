c5967c6c1e1a7571ffa4ade08901c311619b0b57
Antonio Bernardino da Silva <antoniobernardinodasilva@Antonio-S.local>
2022-01-05 15:36:17 -0300

    logRequest added

--- backend/src/shared/http/server.ts
+++ backend/src/shared/http/server.ts
@@ -4,12 +4,13 @@ import express from 'express';
 import 'express-async-errors';
 import '../infra/database/prismaClient';
 import { globalErrors } from './middlewares/globalError';
+import { logRequest } from './middlewares/logRequest';
 
 
 const app = express();
 
 app.use(express.json());
-
+app.use(logRequest)
 app.use(cors())
 app.use(globalErrors)
 
