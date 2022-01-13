16192e7e4b21dc9d72306279405b94ac7e44e564
Antonio Bernardino da Silva <antoniobernardinodasilva@Antonio-S.local>
2022-01-13 19:02:49 -0300

    scripts Eraser added

--- backend/src/shared/http/routes/index.ts
+++ backend/src/shared/http/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { aclRouter } from '../../../modules/acessControlList/infra/http/routes/acl.routes';
 import { authRouter } from '../../../modules/auth/infra/http/routes/auth.routes';
 import { usersRouter } from '../../../modules/user/infra/http/routes/users.routes';
+import '../../scripts/EraserHead';
 
 
 
--- backend/src/shared/http/server.ts
+++ backend/src/shared/http/server.ts
@@ -9,9 +9,11 @@ import { logRequest } from './middlewares/logRequest';
 import routes from './routes';
 
 
+
 const app = express();
 app.use(express.json());
 app.use(cors())
+
 
 app.use(logRequest)
 app.use('/api', routes)
